5506e6e1928d1ce6694b0711d0337d2cc73d62cc
Viktor Tovstyi <viktor.tovstyi@gmail.com>
2025-12-01 16:25:32 +0200

    added "assembly" command, update README.

--- README.md
+++ README.md
@@ -1,1 +1,108 @@
-# Machai
+![](src/site/resources/images/machai-logo.png)
+
+# Machai | Machanism.Org
+
+## Creating and registering Bindex
+
+![](src/site/resources/images/bindex-run.png)
+
+## Assembling the Application
+
+## Run
+
+![](src/site/resources/images/machai-cli-run.png)
+
+## Result
+
+```text
+src/
+  main/
+    java/
+      com/
+        example/
+          loginapp/
+            LoginApplication.java
+            config/
+              UserInteractorConfig.java
+            controller/
+              AuthController.java
+            dto/
+              LoginRequest.java
+              RegisterRequest.java
+    resources/
+      application.properties
+pom.xml
+README.md
+```
+
+```xml
+<!-- pom.xml -->
+<project xmlns="http://maven.apache.org/POM/4.0.0"
+         ...>
+    <modelVersion>4.0.0</modelVersion>
+    <groupId>com.example</groupId>
+    <artifactId>loginapp</artifactId>
+    <version>1.0.0-SNAPSHOT</version>
+    <packaging>jar</packaging>
+    <name>LoginApp</name>
+    <description>Spring Boot application for user login using machanism artifacts.</description>
+
+    <parent>
+        <groupId>org.springframework.boot</groupId>
+        <artifactId>spring-boot-starter-parent</artifactId>
+        <version>3.2.6</version>
+        <relativePath />
+    </parent>
+
+    <properties>
+        <java.version>17</java.version>
+    </properties>
+
+    <dependencies>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-web</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.springframework.boot</groupId>
+            <artifactId>spring-boot-starter-data-jpa</artifactId>
+        </dependency>
+        <dependency>
+            <groupId>org.machanism.macha.core.adapters.spring.web</groupId>
+            <artifactId>user-controller</artifactId>
+            <version>0.0.4-SNAPSHOT</version>
+        </dependency>
+        <dependency>
+            <groupId>org.machanism.macha.core.interactors</groupId>
+            <artifactId>user-interactors</artifactId>
+            <version>0.0.4-SNAPSHOT</version>
+        </dependency>
+        <dependency>
+            <groupId>org.machanism.macha.core.adapters.spring.data</groupId>
+            <artifactId>user-repository-adapter</artifactId>
+            <version>0.0.4-SNAPSHOT</version>
+        </dependency>
+        <dependency>
+            <groupId>org.machanism.macha.core.adapters.spring.web</groupId>
+            <artifactId>jwt-authentication-controller</artifactId>
+            <version>0.0.4-SNAPSHOT</version>
+        </dependency>
+        <!-- H2 for in-memory DB -->
+        <dependency>
+            <groupId>com.h2database</groupId>
+            <artifactId>h2</artifactId>
+            <scope>runtime</scope>
+        </dependency>
+    </dependencies>
+
+    <build>
+        <plugins>
+            <plugin>
+                <groupId>org.springframework.boot</groupId>
+                <artifactId>spring-boot-maven-plugin</artifactId>
+            </plugin>
+        </plugins>
+    </build>
+</project>
+```
+
--- cli/src/main/java/org/machanism/machai/cli/BindexCommand.java
+++ cli/src/main/java/org/machanism/machai/cli/BindexCommand.java
@@ -8,6 +8,7 @@ import java.net.URL;
 import java.util.List;
 
 import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang.StringUtils;
 import org.codehaus.plexus.util.xml.pull.XmlPullParserException;
 import org.machanism.machai.core.Register;
 import org.machanism.machai.core.embedding.EmbeddingProvider;
@@ -20,6 +21,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 
 @ShellComponent
 public class BindexCommand {
+
+	private List<BIndex> bindexList;
+	private String findQuery;
 
 	@ShellMethod()
 	public void bindex(
@@ -36,27 +40,57 @@ public class BindexCommand {
 
 	@ShellMethod()
 	public void find(
-			@ShellOption(value = "query") String query,
+			@ShellOption(value = "The application assembly prompt.") String query,
 			@ShellOption(help = "Max number of artifacts.", value = "limits", defaultValue = "10") int limits)
 			throws IOException {
 
-		try (EmbeddingProvider provider = new EmbeddingProvider("machanism", "bindex")) {
-			List<BIndex> bindexList = provider.search(query, limits);
-
-			URL systemResource = getClass().getResource("/schema/bindex-schema-v1.json");
-			String schema = IOUtils.toString(systemResource, "UTF8");
-
-			StringBuilder assemblPromptBuilder = new StringBuilder(
-					"The bindex schema https://machanism.org/machai/schema/bindex-schema-v1.json:\n" + schema + "\n\n");
-			for (BIndex bindex : bindexList) {
-				System.out.println("ArtifactId: " + bindex.getId());
-				String bindexStr = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(bindex);
-				assemblPromptBuilder.append("```json\n" + bindexStr + "\n```\n\n");
-			}
-
-			try (Writer writer = new FileWriter(new File("assemble.txt"))) {
-				IOUtils.write(assemblPromptBuilder.toString().getBytes(), writer, "UTF8");
-			}
+		findQuery = query;
+		bindexList = getBricks(query, limits);
+		for (BIndex bindex : bindexList) {
+			System.out.println("ArtifactId: " + bindex.getId());
 		}
+	}
+
+	@ShellMethod()
+	public void assembly(
+			@ShellOption(value = "query", defaultValue = "", help = "The application assembly prompt. If empty, an attempt will be made to use the result of the 'find' command, if one was specified previously.") String query,
+			@ShellOption(help = "Max number of artifacts.", value = "limits", defaultValue = "10") int limits)
+			throws IOException {
+
+		String prompt = query;
+
+		if (StringUtils.isBlank(query)) {
+			if (bindexList == null) {
+				throw new IllegalArgumentException("The query is empty.");
+			}
+			prompt = this.findQuery;
+		} else {
+			bindexList = getBricks(query, limits);
+		}
+
+		URL systemResource = getClass().getResource("/schema/bindex-schema-v1.json");
+		String schema = IOUtils.toString(systemResource, "UTF8");
+
+		StringBuilder assemblPromptBuilder = new StringBuilder(
+				"The bindex schema https://machanism.org/machai/schema/bindex-schema-v1.json:\n" + schema + "\n\n");
+		for (BIndex bindex : bindexList) {
+			System.out.println("ArtifactId: " + bindex.getId());
+			String bindexStr = new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(bindex);
+			assemblPromptBuilder.append("```json\n" + bindexStr + "\n```\n\n");
+		}
+
+		assemblPromptBuilder.append(prompt);
+
+		try (Writer writer = new FileWriter(new File("assemble.txt"))) {
+			IOUtils.write(assemblPromptBuilder.toString().getBytes(), writer, "UTF8");
+		}
+	}
+
+	private List<BIndex> getBricks(String query, int limits) throws IOException {
+		List<BIndex> bindexList = null;
+		try (EmbeddingProvider provider = new EmbeddingProvider("machanism", "bindex")) {
+			bindexList = provider.search(query, limits);
+		}
+		return bindexList;
 	}
 }
