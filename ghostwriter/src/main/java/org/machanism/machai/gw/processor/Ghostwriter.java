0e47f91b8014a172dd35ffed04cfff723f5b7bb6
ViktorTovstyi <viktor.tovstyi@gmail.com>
2026-03-10 15:48:25 +0200

    fixed unused import

--- ghostwriter/src/main/java/org/machanism/machai/gw/processor/Ghostwriter.java
+++ ghostwriter/src/main/java/org/machanism/machai/gw/processor/Ghostwriter.java
@@ -2,7 +2,6 @@ package org.machanism.machai.gw.processor;
 
 import java.io.File;
 import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.util.Arrays;
 import java.util.Scanner;
 
