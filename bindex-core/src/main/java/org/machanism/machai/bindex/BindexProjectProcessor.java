2619964bc87ff5a353545235fb5f454db3fc9706
ViktorTovstyi <viktor.tovstyi@gmail.com>
2026-03-20 16:55:55 +0200

    Removed unused SLF4J imports in BindexProjectProcessor.

--- bindex-core/src/main/java/org/machanism/machai/bindex/BindexProjectProcessor.java
+++ bindex-core/src/main/java/org/machanism/machai/bindex/BindexProjectProcessor.java
@@ -6,8 +6,6 @@ import java.io.Reader;
 
 import org.machanism.machai.project.ProjectProcessor;
 import org.machanism.machai.schema.Bindex;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 
