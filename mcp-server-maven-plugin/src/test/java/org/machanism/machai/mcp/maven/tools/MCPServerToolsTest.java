a5bf00fc89b5aed837e79b960b252daa34403bfa
ViktorTovstyi <viktor.tovstyi@gmail.com>
2026-08-30 11:44:02 +0300

    optimized imports.

--- mcp-server-maven-plugin/src/test/java/org/machanism/machai/mcp/maven/tools/MCPServerToolsTest.java
+++ mcp-server-maven-plugin/src/test/java/org/machanism/machai/mcp/maven/tools/MCPServerToolsTest.java
@@ -1,7 +1,7 @@
 package org.machanism.machai.mcp.maven.tools;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.concurrent.CountDownLatch;
