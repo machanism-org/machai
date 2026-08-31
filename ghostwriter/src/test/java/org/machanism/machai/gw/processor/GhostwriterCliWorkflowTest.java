7cfbda8b6cfcea3360b0f9dbd71ab061c77c2c32
ViktorTovstyi <viktor.tovstyi@gmail.com>
2026-08-31 18:21:29 +0300

    optimized imports.

--- ghostwriter/src/test/java/org/machanism/machai/gw/processor/GhostwriterCliWorkflowTest.java
+++ ghostwriter/src/test/java/org/machanism/machai/gw/processor/GhostwriterCliWorkflowTest.java
@@ -4,7 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-import java.io.File;
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 import java.nio.charset.StandardCharsets;
